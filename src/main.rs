c8d9cad9fffaf333e2a8bcbc35f066769c8d1ac2
nurul irfan <irfanlongstride@gmail.com>
2017-10-25 08:07:54 +0700

    tambah verifikasi range 1 - 101

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("the secret number is {}", secret_number);
+
+    loop {
+        println!("please input your guess!");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess {
+            1 ... 101 => {
+                match guess.cmp(&secret_number){
+                    Ordering::Less => println!("Too small"),
+                    Ordering::Greater => println!("Too big"),
+                    Ordering::Equal => {
+                        println!("You win!");
+                        break;
+                    }
+                }
+            }
+            _ => {
+                println!("The number is between 1 - 101", );
+            }
+        }
+    }
+}
